f7a6d8d06294a45a7fc605f880ed1849c06ce380
sakitam-fdd <smilefdd@gmail.com>
2019-05-06 18:40:14 +0800

    feature: ts

--- src/index.ts
+++ src/index.ts
@@ -1,224 +1,376 @@
-// @ts-ignore
-import { Object as OlObj, Map } from 'ol';
-// @ts-ignore
-import { listen, unlistenByKey } from 'ol/events';
+import {
+  max,
+  min,
+  pip,
+  rep,
+  matrixDiag,
+  matrixTranspose,
+  matrixAdd,
+  matrixMultiply,
+  matrixChol,
+  matrixChol2inv,
+  matrixSolve,
+  variogramGaussian,
+  variogramExponential,
+  variogramSpherical,
+} from './utils';
 
-function removeNode(node: HTMLElement) {
-  return node && node.parentNode ? node.parentNode.removeChild(node) : null;
+// Train using gaussian processes with bayesian priors
+function train(t: number[], x: number[], y: number[], model: any, sigma2: any, alpha: number) {
+  const variogram = {
+    t,
+    x,
+    y,
+    nugget: 0.0,
+    range: 0.0,
+    sill: 0.0,
+    A: 1 / 3,
+    n: 0,
+    model: variogramExponential,
+    K: [],
+    M: [],
+  };
+
+  switch (model) {
+    case 'gaussian':
+      variogram.model = variogramGaussian;
+      break;
+    case 'exponential':
+      variogram.model = variogramExponential;
+      break;
+    case 'spherical':
+      variogram.model = variogramSpherical;
+      break;
+    default:
+      variogram.model = variogramExponential;
+  }
+
+  // Lag distance/semivariance
+  let i;
+  let j;
+  let k;
+  let l;
+  let n = t.length;
+  const distance = Array((n * n - n) / 2);
+  for (i = 0, k = 0; i < n; i++) {
+    for (j = 0; j < i; j++, k++) {
+      distance[k] = Array(2);
+      distance[k][0] = Math.pow(
+        Math.pow(x[i] - x[j], 2)
+        + Math.pow(y[i] - y[j], 2), 0.5,
+      );
+      distance[k][1] = Math.abs(t[i] - t[j]);
+    }
+  }
+  distance.sort((a, b) => a[0] - b[0]);
+  variogram.range = distance[(n * n - n) / 2 - 1][0];
+
+  // Bin lag distance
+  const lags = ((n * n - n) / 2) > 30 ? 30 : (n * n - n) / 2;
+  const tolerance = variogram.range / lags;
+  const lag = rep(0, lags);
+  const semi = rep(0, lags);
+  if (lags < 30) {
+    for (l = 0; l < lags; l++) {
+      lag[l] = distance[l][0];
+      semi[l] = distance[l][1];
+    }
+  } else {
+    for (i = 0, j = 0, k = 0, l = 0; i < lags && j < ((n * n - n) / 2); i++, k = 0) {
+      while (distance[j][0] <= ((i + 1) * tolerance)) {
+        lag[l] += distance[j][0];
+        semi[l] += distance[j][1];
+        j++;
+        k++;
+        if (j >= ((n * n - n) / 2)) break;
+      }
+      if (k > 0) {
+        lag[l] /= k;
+        semi[l] /= k;
+        l++;
+      }
+    }
+    if (l < 2) return variogram; // Error: Not enough points
+  }
+
+  // Feature transformation
+  n = l;
+  variogram.range = lag[n - 1] - lag[0];
+  const X = rep(1, 2 * n);
+  const Y = Array(n);
+  const { A } = variogram;
+  for (i = 0; i < n; i++) {
+    // eslint-disable-next-line default-case
+    switch (model) {
+      case 'gaussian':
+        X[i * 2 + 1] = 1.0 - Math.exp(-(1.0 / A) * Math.pow(lag[i] / variogram.range, 2));
+        break;
+      case 'exponential':
+        X[i * 2 + 1] = 1.0 - Math.exp(-(1.0 / A) * lag[i] / variogram.range);
+        break;
+      case 'spherical':
+        X[i * 2 + 1] = 1.5 * (lag[i] / variogram.range)
+          - 0.5 * Math.pow(lag[i] / variogram.range, 3);
+        break;
+    }
+    Y[i] = semi[i];
+  }
+
+  // Least squares
+  const Xt = matrixTranspose(X, n, 2);
+  let Z = matrixMultiply(Xt, X, 2, n, 2);
+  Z = matrixAdd(Z, matrixDiag(1 / alpha, 2), 2, 2);
+  const cloneZ = Z.slice(0);
+  if (matrixChol(Z, 2)) {
+    matrixChol2inv(Z, 2);
+  } else {
+    matrixSolve(cloneZ, 2);
+    Z = cloneZ;
+  }
+  const W = matrixMultiply(matrixMultiply(Z, Xt, 2, 2, n), Y, 2, n, 1);
+
+  // Variogram parameters
+  variogram.nugget = W[0];
+  variogram.sill = W[1] * variogram.range + variogram.nugget;
+  variogram.n = x.length;
+
+  // Gram matrix with prior
+  n = x.length;
+  const K = Array(n * n);
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i; j++) {
+      K[i * n + j] = variogram.model(Math.pow(Math.pow(x[i] - x[j], 2)
+        + Math.pow(y[i] - y[j], 2), 0.5),
+        variogram.nugget,
+        variogram.range,
+        variogram.sill,
+        variogram.A);
+      K[j * n + i] = K[i * n + j];
+    }
+    K[i * n + i] = variogram.model(0, variogram.nugget,
+      variogram.range,
+      variogram.sill,
+      variogram.A);
+  }
+
+  // Inverse penalized Gram matrix projected to target vector
+  let C = matrixAdd(K, matrixDiag(sigma2, n), n, n);
+  const cloneC = C.slice(0);
+  if (matrixChol(C, n)) {
+    matrixChol2inv(C, n);
+  } else {
+    matrixSolve(cloneC, n);
+    C = cloneC;
+  }
+
+  // Copy unprojected inverted matrix as K
+  const K1 = C.slice(0);
+  const M = matrixMultiply(C, t, n, n, 1);
+  // @ts-ignore
+  variogram.K = K1;
+  // @ts-ignore
+  variogram.M = M;
+  return variogram;
 }
 
-const _options = {
-  forcedRerender: false, // Force re-rendering
-  forcedPrecomposeRerender: false, // force pre re-render
-  hideOnZooming: false, // when zooming hide layer
-  hideOnMoving: false, // when moving hide layer
-  hideOnRotating: false, // // when Rotating hide layer
-};
+// Model prediction
+function predict(
+  x: number, y: number,
+  variogram: {
+    n: number;
+    model: (arg0: number, arg1: any, arg2: any, arg3: any, arg4: any) => void;
+    x: number[];
+    y: number[];
+    nugget: any;
+    range: any;
+    sill: any;
+    A: any;
+    M: number[];
+  },
+) {
+  let i;
+  const k = Array(variogram.n);
+  for (i = 0; i < variogram.n; i++) {
+    k[i] = variogram.model(Math.pow(Math.pow(x - variogram.x[i], 2)
+      + Math.pow(y - variogram.y[i], 2), 0.5),
+      variogram.nugget, variogram.range,
+      variogram.sill, variogram.A);
+  }
+  return matrixMultiply(k, variogram.M, 1, variogram.n, 1)[0];
+}
 
-class OverlayLayer extends OlObj {
-  public element: HTMLElement | undefined;
-  public $options: object;
-  private _size: number[] | undefined;
-  private readonly id: any;
-  private readonly insertFirst: Boolean | undefined;
-  private initedEvent: Boolean;
-  private visible: Boolean;
-  constructor(options: object) {
-    super(options);
-
-    this.$options = Object.assign(_options, options);
-
-    // @ts-ignore
-    this.id = options.id;
-
-    /**
-     * size
-     */
-    this._size = undefined;
-
-    /**
-     * event has inited
-     */
-    this.initedEvent = false;
-
-    /**
-     * layer visible
-     */
-    this.visible = true;
-
-    // @ts-ignore
-    this.insertFirst = options.insertFirst !== undefined ? options.insertFirst : true;
-
-    this.render = this.render.bind(this);
-    this.checkUpdate = this.checkUpdate.bind(this);
-    this.createLayerContainer();
-
-    listen(this, 'change:map', this.handleMapChanged, this);
+function variance(
+  x: number, y: number,
+  variogram: {
+    n: number;
+    model: {
+      (arg0: number, arg1: any, arg2: any, arg3: any, arg4: any): void;
+      (arg0: number, arg1: any, arg2: any, arg3: any, arg4: any): number;
+    };
+    x: number[];
+    y: number[];
+    nugget: any;
+    range: any;
+    sill: any;
+    A: any;
+    K: number[];
+  },
+) {
+  let i;
+  const k = Array(variogram.n);
+  for (i = 0; i < variogram.n; i++) {
+    k[i] = variogram.model(
+      Math.pow(Math.pow(x - variogram.x[i], 2) + Math.pow(y - variogram.y[i], 2), 0.5),
+      variogram.nugget,
+      variogram.range,
+      variogram.sill,
+      variogram.A,
+    );
   }
 
-  getElement(): HTMLElement | undefined {
-    return this.element;
-  }
+  const val: number = matrixMultiply(
+    matrixMultiply(k, variogram.K,
+    1, variogram.n, variogram.n),
+    k, 1, variogram.n, 1)[0];
 
-  getId(): any {
-    return this.id;
-  }
+  // @ts-ignore
+  return variogram.model(0, variogram.nugget, variogram.range, variogram.sill, variogram.A) + val;
+}
 
-  setMap(map: any): void {
-    this.set('map', map);
-  }
+// Gridded matrices or contour paths
+function grid(
+  polygons: number[][],
+  variogram: {
+    t: number[];
+    n: number;
+    model: (arg0: number, arg1: any, arg2: any, arg3: any, arg4: any) => void;
+    x: number[];
+    y: number[];
+    nugget: any;
+    range: any;
+    sill: any;
+    A: any;
+    M: number[];
+  },
+  width: number,
+) {
+  let i;
+  let j;
+  let k;
+  const n = polygons.length;
+  if (n === 0) return;
 
-  getMap() {
-    return this.get('map');
-  }
-
-  /**
-   * create container
-   */
-  createLayerContainer () {
-    const container = (this.element = document.createElement('div'));
-    container.style.position = 'absolute';
-    container.style.top = '0px';
-    container.style.left = '0px';
-    container.style.right = '0px';
-    container.style.bottom = '0px';
-  }
-
-  /**
-   * handle map changed
-   */
-  handleMapChanged () {
-    if (this.initedEvent) {
-      this.element && removeNode(this.element);
-      this.unBindEvent();
+  // Boundaries of polygons space
+  const xlim = [polygons[0][0][0], polygons[0][0][0]];
+  const ylim = [polygons[0][0][1], polygons[0][0][1]];
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < polygons[i].length; j++) { // Vertices
+      if (polygons[i][j][0] < xlim[0]) xlim[0] = polygons[i][j][0];
+      if (polygons[i][j][0] > xlim[1]) xlim[1] = polygons[i][j][0];
+      if (polygons[i][j][1] < ylim[0]) ylim[0] = polygons[i][j][1];
+      if (polygons[i][j][1] > ylim[1]) ylim[1] = polygons[i][j][1];
     }
-    const map = this.getMap();
-    if (map) {
-      this.bindEvent(map);
-      const container = map.getOverlayContainer();
-      if (this.insertFirst) {
-        container.insertBefore(this.element, container.childNodes[0] || null);
-      } else {
-        container.appendChild(this.element);
+  }
+
+  // Alloc for O(n^2) space
+  let xtarget;
+  let ytarget;
+  const a = Array(2);
+  const b = Array(2);
+  const lxlim = Array(2); // Local dimensions
+  const lylim = Array(2); // Local dimensions
+  const x = Math.ceil((xlim[1] - xlim[0]) / width);
+  const y = Math.ceil((ylim[1] - ylim[0]) / width);
+  const A = Array(x + 1);
+  for (i = 0; i <= x; i++) A[i] = Array(y + 1);
+  for (i = 0; i < n; i++) {
+    // Range for polygons[i]
+    lxlim[0] = polygons[i][0][0];
+    lxlim[1] = lxlim[0];
+    lylim[0] = polygons[i][0][1];
+    lylim[1] = lylim[0];
+    for (j = 1; j < polygons[i].length; j++) { // Vertices
+      if (polygons[i][j][0] < lxlim[0]) lxlim[0] = polygons[i][j][0];
+      if (polygons[i][j][0] > lxlim[1]) lxlim[1] = polygons[i][j][0];
+      if (polygons[i][j][1] < lylim[0]) lylim[0] = polygons[i][j][1];
+      if (polygons[i][j][1] > lylim[1]) lylim[1] = polygons[i][j][1];
+    }
+
+    // Loop through polygon subspace
+    a[0] = Math.floor(((lxlim[0] - ((lxlim[0] - xlim[0]) % width)) - xlim[0]) / width);
+    a[1] = Math.ceil(((lxlim[1] - ((lxlim[1] - xlim[1]) % width)) - xlim[0]) / width);
+    b[0] = Math.floor(((lylim[0] - ((lylim[0] - ylim[0]) % width)) - ylim[0]) / width);
+    b[1] = Math.ceil(((lylim[1] - ((lylim[1] - ylim[1]) % width)) - ylim[0]) / width);
+    for (j = a[0]; j <= a[1]; j++) {
+      for (k = b[0]; k <= b[1]; k++) {
+        xtarget = xlim[0] + j * width;
+        ytarget = ylim[0] + k * width;
+        if (pip(polygons[i], xtarget, ytarget)) {
+          A[j][k] = predict(xtarget, ytarget, variogram);
+        }
       }
     }
   }
+  return {
+    xlim,
+    ylim,
+    width,
+    data: A,
+    zlim: [min(variogram.t), max(variogram.t)],
+  };
+}
 
-  /**
-   * bind map update
-   */
-  render(): void {
-    this.dispatchEvent({
-      type: 'render',
-      value: this,
-    });
-  }
-
-  prerender(): void {
-    this.dispatchEvent({
-      type: 'prerender',
-      value: this,
-    });
-  }
-
-  /**
-   * update view size
-   * @param size
-   */
-  updateViewSize(size: number[]): void {
-    if (!this.element) return;
-    this._size = size;
-    this.element.style.width = `${size[0]}px`;
-    this.element.style.height = `${size[1]}px`;
-    this.element.setAttribute('width', String(size[0]));
-    this.element.setAttribute('height', String(size[1]));
-  }
-
-  /**
-   * update layer
-   */
-  checkUpdate() {
-    const map = this.getMap();
-    if (!map || !map.isRendered()) {
-      this.setVisible(false);
-      return;
+// Plotting on the DOM
+function plot(
+  canvas: HTMLCanvasElement,
+  grid: {
+    data: [][],
+    xlim: number,
+    ylim: number,
+    width: number,
+    zlim: number,
+  },
+  xlim: number[],
+  ylim: number[],
+  colors: any[],
+) {
+  // Clear screen
+  const ctx = canvas.getContext('2d');
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Starting boundaries
+    const range = [xlim[1] - xlim[0], ylim[1] - ylim[0], grid.zlim[1] - grid.zlim[0]];
+    let i;
+    let j;
+    let x;
+    let y;
+    let z;
+    const n = grid.data.length;
+    const m = grid.data[0].length;
+    // @ts-ignore
+    const wx = Math.ceil(grid.width * canvas.width / (xlim[1] - xlim[0]));
+    // @ts-ignore
+    const wy = Math.ceil(grid.width * canvas.height / (ylim[1] - ylim[0]));
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < m; j++) {
+        // eslint-disable-next-line no-continue
+        if (grid.data[i][j] === undefined) continue;
+        x = canvas.width * (i * grid.width + grid.xlim[0] - xlim[0]) / range[0];
+        y = canvas.height * (1 - (j * grid.width + grid.ylim[0] - ylim[0]) / range[1]);
+        z = (grid.data[i][j] - grid.zlim[0]) / range[2];
+        if (z < 0.0) z = 0.0;
+        if (z > 1.0) z = 1.0;
+        ctx.fillStyle = colors[Math.floor((colors.length - 1) * z)];
+        ctx.fillRect(Math.round(x - wx / 2), Math.round(y - wy / 2), wx, wy);
+      }
     }
-
-    const mapSize = map.getSize();
-    if (!this._size) {
-      this.updateViewSize(mapSize);
-    } else {
-      const _update = this._size[0] === 0 || this._size[1] === 0
-        || (this._size[0] !== mapSize[0] || this._size[1] !== mapSize[1]);
-      _update && this.updateViewSize(mapSize);
-    }
-  }
-
-  setVisible(visible: Boolean): void {
-    if (this.visible !== visible) {
-      // @ts-ignore
-      this.element.style.display = visible ? '' : 'none';
-      this.visible = visible;
-    }
-    this.dispatchEvent({
-      type: 'change:visible ',
-      value: visible,
-    });
-  }
-
-  /**
-   * is visible
-   * @returns {Element|*|boolean}
-   * @private
-   */
-  isVisible (): Boolean {
-    return this.visible;
-  }
-
-  dispatchEvent(...args: any) {
-    super.dispatchEvent(...args);
-  }
-
-  show() {
-    this.setVisible(true);
-  }
-
-  hide() {
-    this.setVisible(false);
-  }
-
-  private bindEvent(map: any) {
-    // const view = map.getView();
-    // map.on('rendercomplete', this.render);
-    map.on('change:size', this.checkUpdate);
-    // view.on('change:center', this.render);
-    // view.on('change:resolution', this.render);
-    // view.on('change:rotation', this.render);
-    // map.on('postcompose', this.render);
-    // map.on('postrender', this.render_);
-    map.on('precompose', this.render);
-    // map.on('movestart', this.render);
-    // map.on('moveend', this.render);
-    this.initedEvent = true;
-  }
-
-  private unBindEvent() {
-    const map = this.getMap();
-    // map.un('rendercomplete', this.render);
-    // map.un('postcompose', this.render);
-    // map.un('postrender', this.render);
-    map.un('precompose', this.render);
-    map.un('change:size', this.checkUpdate);
-    this.initedEvent = false;
-  }
-
-  private set(key: string, value: any) {
-    return super.set(key, value);
-  }
-
-  private get(key: string) {
-    return super.get(key);
   }
 }
 
-export default OverlayLayer;
+export default {
+  train,
+  predict,
+  variance,
+  grid,
+  plot,
+};
---
+++ src/utils.ts
@@ -0,0 +1,283 @@
+// https://en.wikipedia.org/wiki/Duff's_device
+// https://v8.dev/blog/elements-kinds
+/**
+ * search array max value
+ * @param source
+ */
+function max(source: number[]): number {
+  return Math.max.apply(null, source);
+}
+
+/**
+ * search array min value
+ * @param source
+ */
+function min(source: number[]): number {
+  return Math.min.apply(null, source);
+}
+
+/**
+ * get mean value from array number
+ * @param source
+ */
+function mean(source: number[]): number {
+  let i = 0;
+  let sum = 0;
+  const length = source.length;
+  for (; i < length; i++) sum += source[i];
+  return sum / source.length;
+}
+
+/**
+ * fill array with number
+ * @param source
+ * @param n
+ */
+function rep(source: number, n: number): number[] {
+  // https://v8.dev/blog/elements-kinds
+  const array = [];
+  for (let i = 0; i < n; i++) {
+    array.push(source);
+  }
+  return array;
+  // return (new Array(n)).fill(source);
+}
+
+function pip(source: number[], x: number, y: number): boolean {
+  let i = 0;
+  let j = source.length - 1;
+  let c = false;
+  const length = source.length;
+  for (; i < length; j = i++) {
+    if (((source[i][1] > y) !== (source[j][1] > y))
+      && (x < (source[j][0] - source[i][0]) * (y - source[i][1]) / (source[j][1] - source[i][1]) + source[i][0])) {
+      c = !c;
+    }
+  }
+  return c;
+}
+
+// Matrix algebra
+function matrixDiag(c: number, n: number): number[] {
+  let i;
+  const Z = rep(0, n * n);
+  for (i = 0; i < n; i++) Z[i * n + i] = c;
+  return Z;
+}
+
+function matrixTranspose(X: any[], n: number, m: number): any[] {
+  let i = 0;
+  let j = 0;
+  const Z = Array(m * n);
+  for (; i < n; i++) for (; j < m; j++) Z[j * n + i] = X[i * m + j];
+  return Z;
+}
+
+function matrixScale(X: number[], c: number, n: number, m: number) {
+  let i = 0;
+  let j = 0;
+  for (; i < n; i++) for (; j < m; j++) X[i * m + j] *= c;
+}
+
+function matrixAdd(X: number[], Y: number[], n: number, m: number): number[] {
+  let i = 0;
+  let j = 0;
+  const Z = Array(n * m);
+  for (; i < n; i++) for (; j < m; j++) Z[i * m + j] = X[i * m + j] + Y[i * m + j];
+  return Z;
+}
+
+// Naive matrix multiplication
+function matrixMultiply(X: number[], Y: number[], n: number, m: number, p: number): number[] {
+  let i = 0;
+  const Z = Array(n * p);
+  for (; i < n; i++) {
+    let j = 0;
+    for (; j < p; j++) {
+      Z[i * p + j] = 0;
+      let k = 0;
+      for (; k < m; k++) Z[i * p + j] += X[i * m + k] * Y[k * p + j];
+    }
+  }
+  return Z;
+}
+
+// Cholesky decomposition
+function matrixChol(X: number[], n: number): boolean {
+  let i;
+  let j;
+  let k;
+  const p = Array(n);
+  for (i = 0; i < n; i++) p[i] = X[i * n + i];
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i; j++) p[i] -= X[i * n + j] * X[i * n + j];
+    if (p[i] <= 0) return false;
+    p[i] = Math.sqrt(p[i]);
+    for (j = i + 1; j < n; j++) {
+      for (k = 0; k < i; k++) X[j * n + i] -= X[j * n + k] * X[i * n + k];
+      X[j * n + i] /= p[i];
+    }
+  }
+  for (i = 0; i < n; i++) X[i * n + i] = p[i];
+  return true;
+}
+
+// Inversion of cholesky decomposition
+function matrixChol2inv(X: number[], n: number) {
+  let i;
+  let j;
+  let k;
+  let sum;
+  for (i = 0; i < n; i++) {
+    X[i * n + i] = 1 / X[i * n + i];
+    for (j = i + 1; j < n; j++) {
+      sum = 0;
+      for (k = i; k < j; k++) sum -= X[j * n + k] * X[k * n + i];
+      X[j * n + i] = sum / X[j * n + j];
+    }
+  }
+  for (i = 0; i < n; i++) for (j = i + 1; j < n; j++) X[i * n + j] = 0;
+  for (i = 0; i < n; i++) {
+    X[i * n + i] *= X[i * n + i];
+    for (k = i + 1; k < n; k++) X[i * n + i] += X[k * n + i] * X[k * n + i];
+    for (j = i + 1; j < n; j++) for (k = j; k < n; k++) X[i * n + j] += X[k * n + i] * X[k * n + j];
+  }
+  for (i = 0; i < n; i++) for (j = 0; j < i; j++) X[i * n + j] = X[j * n + i];
+}
+
+// Inversion via gauss-jordan elimination
+function matrixSolve(X: number[], n: number) {
+  const m = n;
+  const b = Array(n * n);
+  const indxc = Array(n);
+  const indxr = Array(n);
+  const ipiv = Array(n);
+  let i;
+  let icol: number = 0;
+  let irow: number = 0;
+  let j;
+  let k;
+  let l;
+  let ll;
+  let big;
+  let dum;
+  let pivinv;
+  let temp;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      if (i === j) b[i * n + j] = 1;
+      else b[i * n + j] = 0;
+    }
+  }
+
+  for (j = 0; j < n; j++) ipiv[j] = 0;
+
+  for (i = 0; i < n; i++) {
+    big = 0;
+    for (j = 0; j < n; j++) {
+      if (ipiv[j] !== 1) {
+        for (k = 0; k < n; k++) {
+          if (ipiv[k] === 0) {
+            if (Math.abs(X[j * n + k]) >= big) {
+              big = Math.abs(X[j * n + k]);
+              irow = j;
+              icol = k;
+            }
+          }
+        }
+      }
+    }
+
+    ++(ipiv[icol]);
+
+    if (irow !== icol) {
+      for (l = 0; l < n; l++) {
+        temp = X[irow * n + l];
+        X[irow * n + l] = X[icol * n + l];
+        X[icol * n + l] = temp;
+      }
+      for (l = 0; l < m; l++) {
+        temp = b[irow * n + l];
+        b[irow * n + l] = b[icol * n + l];
+        b[icol * n + l] = temp;
+      }
+    }
+    indxr[i] = irow;
+    indxc[i] = icol;
+
+    if (X[icol * n + icol] === 0) return false; // Singular
+
+    pivinv = 1 / X[icol * n + icol];
+    X[icol * n + icol] = 1;
+    for (l = 0; l < n; l++) X[icol * n + l] *= pivinv;
+    for (l = 0; l < m; l++) b[icol * n + l] *= pivinv;
+
+    for (ll = 0; ll < n; ll++) {
+      if (ll !== icol) {
+        dum = X[ll * n + icol];
+        X[ll * n + icol] = 0;
+        for (l = 0; l < n; l++) X[ll * n + l] -= X[icol * n + l] * dum;
+        for (l = 0; l < m; l++) b[ll * n + l] -= b[icol * n + l] * dum;
+      }
+    }
+  }
+  for (l = (n - 1); l >= 0; l--) {
+    if (indxr[l] !== indxc[l]) {
+      for (k = 0; k < n; k++) {
+        temp = X[k * n + indxr[l]];
+        X[k * n + indxr[l]] = X[k * n + indxc[l]];
+        X[k * n + indxc[l]] = temp;
+      }
+    }
+  }
+
+  return true;
+}
+
+// Variogram models
+function variogramGaussian(
+  h: number, nugget: number,
+  range: number, sill: number,
+  A: number,
+) {
+  return nugget + ((sill - nugget) / range)
+    * (1.0 - Math.exp(-(1.0 / A) * Math.pow(h / range, 2)));
+}
+
+function variogramExponential(
+  h: number, nugget: number,
+  range: number, sill: number,
+  A: number,
+) {
+  return nugget + ((sill - nugget) / range)
+    * (1.0 - Math.exp(-(1.0 / A) * (h / range)));
+}
+
+function variogramSpherical(
+  h: number, nugget: number,
+  range: number, sill: number,
+) {
+  if (h > range) return nugget + (sill - nugget) / range;
+  return nugget + ((sill - nugget) / range)
+    * (1.5 * (h / range) - 0.5 * Math.pow(h / range, 3));
+}
+
+export {
+  max,
+  min,
+  pip,
+  rep,
+  mean,
+  matrixDiag,
+  matrixTranspose,
+  matrixScale,
+  matrixAdd,
+  matrixMultiply,
+  matrixChol,
+  matrixChol2inv,
+  matrixSolve,
+  variogramGaussian,
+  variogramExponential,
+  variogramSpherical,
+};
